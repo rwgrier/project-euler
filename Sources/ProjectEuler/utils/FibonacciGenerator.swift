52f4b612a8cf6f3f8d780a52f2b27e9a9e72f521
Ryan Grier <rwgrier@gmail.com>
2025-04-10 20:11:38 -0400

    Adding a Swiftlint GitHub Action
    Ran swiftlint and fixed the issues.

--- Sources/ProjectEuler/utils/FibonacciGenerator.swift
+++ Sources/ProjectEuler/utils/FibonacciGenerator.swift
@@ -1,16 +1,16 @@
 final class FibonacciGenerator {
     private var memoized: [Int: Int] = [:]
-    
+
     func fibonacci(at index: Int) -> Int {
         if let fibonacci = memoized[index] {
             return fibonacci
         }
-        
+
         guard index >= 2 else {
             memoized[index] = index
             return index
         }
-        
+
         let fibonacci = fibonacci(at: index - 1) + fibonacci(at: index - 2)
         memoized[index] = fibonacci
         return fibonacci
--- Tests/ProjectEulerTests/Problem002Tests.swift
+++ Tests/ProjectEulerTests/Problem002Tests.swift
@@ -4,18 +4,18 @@ import Testing
 @Suite("Problem 002")
 struct Problem002Tests {
     var subject: Problem002 = .init()
-    
+
     @Test("Known even Fibonacci sum below 89.")
     func test_knownValue() {
         let test = subject.sumEvenFibonacci(89)
-        
+
         #expect(test == 44)
     }
-    
+
     @Test("Test even Fibonacci sum below 4,000,000.")
     func test_testValue() {
         let test = subject.sumEvenFibonacci(4_000_000)
-        
+
         #expect(test == 4613732)
     }
 }
